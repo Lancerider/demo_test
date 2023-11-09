7e8476372f6f2c4186b713dac42ecb8a705ee583
Jorge Palacios <jorge.p@scopicsoftware.com>
2023-11-09 19:05:53 -0300

    :card_file_box: tables migrations

--- database/migrations/2023_11_08_133846_create_demo_test_inquiry_table.php
+++ database/migrations/2023_11_08_133846_create_demo_test_inquiry_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('demo_test_inquiry', function (Blueprint $table) {
             $table->id();
+
+            $table->json('payload');
+            $table->enum('status', ['ACTIVE', 'PROCESSED', 'FAILED'])->default('ACTIVE');
+            $table->unsignedInteger('items_total_count')->nullable()->default(null);
+            $table->unsignedInteger('items_processed_count')->default(0);
+            $table->unsignedInteger('items_failed_count')->default(0);
+
             $table->timestamps();
         });
     }
